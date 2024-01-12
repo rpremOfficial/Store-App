57f5ba46d191444f6759d24c5da3621f27a661b1
Prem Kumar <premraj259@outlook.com>
2024-01-13 01:21:52 +0530

    Initial commit

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,64 @@
+using GameStore.Api.Entities;
+
+const string GetGameEndpointName = "GetGame";
+
+List<Game> games = new(){
+    new Game(){
+        ID = 1,
+        Name = "FIFA 22",
+        Genre = "Sports",
+        Price = 299.99M,
+        ReleaseDate = new DateTime(2021, 10, 1),
+        ImageUri = "https://placehold.it/100x100"
+    },
+    new Game(){
+        ID = 2,
+        Name = "FIFA 21",
+        Genre = "Sports",
+        Price = 199.99M,
+        ReleaseDate = new DateTime(2020, 10, 1),
+        ImageUri = "https://placehold.it/100x100"
+    },
+    new Game(){
+        ID = 3,
+        Name = "FIFA 20",
+        Genre = "Sports",
+        Price = 99.99M,
+        ReleaseDate = new DateTime(2019, 10, 1),
+        ImageUri = "https://placehold.it/100x100"
+    },
+    new Game(){
+        ID = 4,
+        Name = "FIFA 19",
+        Genre = "Sports",
+        Price = 49.99M,
+        ReleaseDate = new DateTime(2018, 10, 1),
+        ImageUri = "https://placehold.it/100x100"
+    }
+};
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+// Get requests
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/games", () => games);
+
+app.MapGet("/games/{id}", (int id) =>
+    {
+        Game? game = games.Find(game => game.ID == id);
+
+        if (game is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(game);
+    }
+)
+.WithName(GetGameEndpointName);
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,2 @@
+# Store App
+ A full stack store web app based on .NET RestAPI
