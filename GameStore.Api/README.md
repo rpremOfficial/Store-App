746c22e9c5e13255425c56c6fa3c5649df204ba5
Prem Kumar <premraj259@outlook.com>
2024-01-23 15:13:22 +0530

    added docker and connection string

---
+++ GameStore.Api/README.md
@@ -0,0 +1,9 @@
+# Game Store API
+
+## Satrting SQL Server
+
+    ```powershell
+        $sa_password = "[SA Password here]"
+
+        docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=$sa_password" -e "MSSQL_PID=Evaluation" -p 1433:1433  --name mssql --hostname sqlpreview -v sqlvolume:/var/opt/mssql -d --rm mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04
+    ```
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d281f013f064ad4c77c4898bb0e651f9e7956340")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24f58f4a198dfda3af45a8cfb5ecbebdf38cd4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
